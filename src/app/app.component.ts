f2cb4427b2d164373eb19ea1ba6c37add8cc7b90
RushikeshDesaiRD <rushikeshdesaitwo@gmail.com>
2020-11-14 02:03:55 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { UserService } from './serivces/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'random-card';
+export class AppComponent implements OnInit {
+  user:any
+  constructor(
+    private userSevice:UserService,
+    private tostr:ToastrService
+    ){
+
+  }
+  ngOnInit(): void {
+    this.userSevice.getData().subscribe(
+      (user:any)=>{
+       
+        this.user=user.results[0];
+        console.log(this.user)
+      },
+      (err)=>{
+        this.tostr.error(err.status, "Something Went Wrong")
+      }
+      
+
+    )  
+    
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
-
+import { CardComponent } from './card/card.component';
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
